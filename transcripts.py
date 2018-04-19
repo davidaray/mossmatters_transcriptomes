45fec4ec74db8f7acfcb61c21dec7d2fd6a5ed4f
David Ray <daray@quanah.hpcc.ttu.edu>
2018-04-18 19:50:57 -0500

    initial commit working archive version

---
+++ transcripts.py
@@ -0,0 +1,57 @@
+
+from Bio import SeqIO
+import statistics as stat
+import pandas as pd
+import argparse
+
+def get_args():
+	parser = argparse.ArgumentParser(description="Python seminar assignment. Will parse transcriptome data.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('-i', '--ifasta', type=str, help='Name of the peptide file', required=True)
+	parser.add_argument('-b', '--blastx', type=str, help='Name of the blastx file', required=True)
+
+	args = parser.parse_args()
+	INFASTA = args.ifasta
+	BLASTX = args.blastx
+	
+	return INFASTA, BLASTX
+#	return INFASTA, PREFIX
+INFASTA, BLASTX = get_args()
+
+print("Input fasta = " + INFASTA + ".")
+print("Input blastx = " + BLASTX + ".")
+
+ID_LIST = []
+LENLIST = []
+IDLIST = []
+
+with open('stats.txt', 'w') as STATS:
+
+#TD = SeqIO.to_dict(SeqIO.parse(INFASTA, "fasta"))
+
+	for RECORD in SeqIO.parse(INFASTA, "fasta"):
+		LENLIST.append(len(RECORD.seq))
+		IDLIST.append(RECORD.id)
+		for LINE in IDLIST:
+			LINE = LINE.rsplit("_",1)
+			ID_LIST.append(LINE)
+			
+#print(LIST)
+	print('Total bases = ' + str(sum(LENLIST)) + '.' )
+	STATS.write('Total bases = ' + str(sum(LENLIST)) + '.\n')
+	print('Total transcripts = ' + str(len(LENLIST)) + '.')
+	STATS.write('Total transcripts = ' + str(len(LENLIST)) + '.\n')
+	print('Mean length of transcripts = ' + str(stat.mean(LENLIST)) + '.')
+	STATS.write('Mean length of transcripts = ' + str(stat.mean(LENLIST)) + '.\n')
+	print('Median length of transcripts = ' + str(stat.median(LENLIST)) + '.')
+	STATS.write('Median length of transcripts = ' + str(stat.median(LENLIST)) + '.\n')
+#STATS.close()
+
+IDS = pd.DataFrame(ID_LIST, columns = ['uni', 'iso'])
+#print(IDS)
+COUNTUNI = len(IDS['uni'].unique())
+print('There are ' + str(COUNTUNI) + ' unigenes in the file.')
+
+
+
+
+
