91bffb0ddb8f4d116b6c5ad7d092d2410cb3bccb
David Ray <daray@compute-1-25.localdomain>
2018-04-21 09:49:32 -0500

    changed prefix option

--- transcripts.py
+++ transcripts.py
@@ -8,23 +8,25 @@ def get_args():
 	parser = argparse.ArgumentParser(description="Python seminar assignment. Will parse transcriptome data.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-i', '--ifasta', type=str, help='Name of the peptide file', required=True)
 	parser.add_argument('-b', '--blastx', type=str, help='Name of the blastx file', required=True)
+	parser.add_argument('-p', '--prefix', type=str, help='A prefix to label the output', required=True)
 
 	args = parser.parse_args()
 	INFASTA = args.ifasta
 	BLASTX = args.blastx
-	
-	return INFASTA, BLASTX
-#	return INFASTA, PREFIX
-INFASTA, BLASTX = get_args()
+	PREFIX = args.prefix
+
+	return INFASTA, BLASTX, PREFIX
+INFASTA, BLASTX, PREFIX = get_args()
 
 print("Input fasta = " + INFASTA + ".")
 print("Input blastx = " + BLASTX + ".")
+print("Prefix = " + PREFIX + ".")
 
 ID_LIST = []
 LENLIST = []
 IDLIST = []
 
-with open('stats.txt', 'w') as STATS:
+with open(PREFIX + '.stats.txt', 'w') as STATS:
 
 
 	for RECORD in SeqIO.parse(INFASTA, 'fasta'):
@@ -40,10 +42,11 @@ with open('stats.txt', 'w') as STATS:
 	STATS.write('Median length of transcripts = ' + str(stat.median(LENLIST)) + '.\n')
 	
 	LENLIST = sorted(LENLIST)
+	MEAN = stat.mean(LENLIST)
 	TOTALENTRIES = len(LENLIST)
 	for ENTRY in range(1, TOTALENTRIES):
 		RUNNINGTOTAL = sum(LENLIST[0:ENTRY])
-		if RUNNINGTOTAL >= sum(LENLIST)/2:
+		if RUNNINGTOTAL >= MEAN:
 			print('N50 = ' + str(LENLIST[ENTRY-1]) + '.')
 			STATS.write('N50 = ' + str(LENLIST[ENTRY-1]) + '.\n')
 			break
